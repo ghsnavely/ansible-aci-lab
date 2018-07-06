a26871a55ee1471591ad9f170a79976c0feb144b
Garrett Snavely <ghsnavely@gmail.com>
2018-07-06 14:44:26 -0500

    test
    building a basic test

---
+++ ciscoACIvmmTesting/ping_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#test ping setup
+
+date
+cat ./ip_list.txt | while read output
+do
+    ping -c 1 "$output" > /dev/null
+    if [ $Q -eq 0 ]; then
+    echo "node $output is up"
+    else
+    echo "note $output is down"
+    fi
+done
