6c0638bffd65792e16cb4719cbdeb4155a20d34b
Garrett Snavely <ghsnavely@gmail.com>
2018-07-06 15:09:49 -0500

    bash script change

--- ciscoACIvmmTesting/ping_test_10.sh
+++ ciscoACIvmmTesting/ping_test_10.sh
@@ -6,7 +6,7 @@ date
 cat ./ip_list_10.txt | while read output
 do
     ping -c 1 "$output" > /dev/null
-    if [ $Q -eq 0 ]; then
+    if [ $? -eq 0 ]; then
     echo "node $output is up"
     else
     echo "note $output is down"
